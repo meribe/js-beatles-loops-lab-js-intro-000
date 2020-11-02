2147331742f9de88845e7a62e891a208f2e53ec6
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-11-02 18:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+var emptyArray= [];
+function theBeatlesPlay(arrayMus, arrayIns){
+  for (let i=0; i<arrayMus.length; i++){
+  emptyArray[i]=`${arrayMus[i]} plays ${arrayIns[i]}`
+}
+  return emptyArray
+}
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"];
+function johnLennonFacts(facts){
+var excFacts=[];
+var i=0
+
+  while (i< facts.length);{
+    excFacts[i]= `${facts[i]}!!!`
+   i++
+  }
+  return excFacts
+}
+
+
+function iLoveTheBeatles(num){
+  var empty= [];
+  do{
+    empty.push("I love the Beatles")
+  }
+  while (num< parseInt(num))
+  return empty
+}
